f6b336854f1bd56173646a6da350860e7ba29a83
Kamigami55 <samkami@icloud.com>
2017-06-26 11:54:45 +0800

    Done chapter 5

---
+++ 0501-inventory.py
@@ -0,0 +1,14 @@
+# inventory.py
+
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(v, end=' ')
+        print(k)
+        item_total = item_total + v
+    print("Total number of items: " + str(item_total))
+
+displayInventory(stuff)
---
+++ 0502-inventory2.py
@@ -0,0 +1,24 @@
+# inventory2.py
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(v, end=' ')
+        print(k)
+        item_total = item_total + v
+    print("Total number of items: " + str(item_total))
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        if item in inventory:
+            inventory[item] = inventory[item] + 1
+        else:
+            inventory[item] = 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
